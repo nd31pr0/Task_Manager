dfe3bea1ff31d7ec514c9aaa9bba85ba3517e3ac
Cyndzi <cyndzi@Cyndzis-MBP.bbrouter>
2023-08-04 06:49:30 +0100

    Create operation for tasks implemented and tested

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,48 @@
+const dotenv = require('dotenv').config();
 const express = require('express');
+const connectDB = require('./config/connectDB');
+const Task = require('./model/taskModel');
+
+
+const app = express();
+
+//Middleware
+app.use(express.json())
+// const logger = (req, res, next) => {
+//     console.log('MiddleWare ran')
+//     console.log(req.method);
+//     next();
+// }
+
+// Routes    
+app.get("/", (req,res) => {
+    res.send("Home Page")
+})
+
+//Create a task: 
+app.post('/api/tasks', async (req, res) => {
+   
+   try {
+    const task = await Task.create(req.body)
+    res.status(200).json(task)
+   } catch (error) {
+    res.status(500).json({msg: error.message})
+   }
+
+})
+
+const PORT = process.env.PORT || 5000;
+
+
+
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.log(error);
+}
+};
+startServer();
